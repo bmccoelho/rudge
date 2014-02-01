6f1ee5802d1772d97d222cebc7141898a7041996
bruno coelho <bruno.coelho@gmail.com>
2014-02-01 23:10:11 +0000

    adds list of abbreviations

---
+++ lib/rudge/abbreviations.rb
@@ -0,0 +1,22 @@
+class Rudge
+  class Abbreviations
+    TITLES = %w(jr, mr, mrs, ms, dr, prof, sr, sen, rep, rev, gov, atty,
+                supt, det, rev, col, gen, lt, cmdr, adm, capt, sgt, cpl, maj)
+
+    ENTITIES = %w(dept, univ, uni, assn, bros, inc, ltd, co, corp, plc)
+
+    MONTHS = %w(jan, feb, mar, apr, may, jun, jul, aug, sep, sept, oct,
+                nov, dec)
+
+    DAYS = %w(mon, tue, wed, thu, fri, sat, sun)
+
+    ADDRESSES = %w(ave, bld, blvd, cl, ct, cres, dr, rd, st)
+
+    MISC = %w(vs, etc, no, esp, cf)
+
+    def self.list
+      TITLES + ENTITIES + MONTHS + DAYS + ADDRESSES + MISC
+    end
+
+  end
+end
--- rudge.gemspec
+++ rudge.gemspec
@@ -6,7 +6,7 @@ Gem::Specification.new do |s|
   s.description = "A Ruby Judge! Determines the sentences in a text."
   s.authors     = ["bruno coelho"]
   s.email       = 'bruno.coelho@gmail.com'
-  s.files       = ["lib/rudge.rb"]
+  s.files       = ["lib/rudge.rb", "lib/rudge/abbreviations.rb"]
   s.homepage    =
     'http://rubygems.org/gems/rudge'
   s.license       = 'MIT'
