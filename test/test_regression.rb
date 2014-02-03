3172493f9eeb4bab7ee90b5f5f951f01c4a409d6
bruno coelho <bruno.coelho@gmail.com>
2014-02-03 17:13:05 +0000

    adds regression tests

---
+++ test/test_regression.rb
@@ -0,0 +1,12 @@
+require "test/unit"
+require "rudge"
+
+class RudgeRegressionTest < Test::Unit::TestCase
+  def test_regression
+    sentences = Rudge.sentences("Hello Project. Goodbye.")
+
+    assert_equal 2,                sentences.size
+    assert_equal "Hello Project.", sentences[0]
+    assert_equal "Goodbye.",       sentences[1]
+  end
+end
