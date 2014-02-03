da321e03028819f0c63f0f0b6200ea9de6402c78
bruno coelho <bruno.coelho@gmail.com>
2014-02-03 11:35:56 +0000

    fixes list of abbreviations and adds capitalized abbreviations

--- lib/rudge/abbreviations.rb
+++ lib/rudge/abbreviations.rb
@@ -1,21 +1,21 @@
 class Rudge
   class Abbreviations
-    TITLES = %w(jr, mr, mrs, ms, dr, prof, sr, sen, rep, rev, gov, atty,
-                supt, det, rev, col, gen, lt, cmdr, adm, capt, sgt, cpl, maj)
+    TITLES = %w(jr mr mrs ms dr prof sr sen rep rev gov atty
+                supt det rev col gen lt cmdr adm capt sgt cpl maj)
 
-    ENTITIES = %w(dept, univ, uni, assn, bros, inc, ltd, co, corp, plc)
+    ENTITIES = %w(dept univ uni assn bros inc ltd co corp plc)
 
-    MONTHS = %w(jan, feb, mar, apr, may, jun, jul, aug, sep, sept, oct,
-                nov, dec)
+    MONTHS = %w(jan feb mar apr may jun jul aug sep sept oct nov dec)
 
-    DAYS = %w(mon, tue, wed, thu, fri, sat, sun)
+    DAYS = %w(mon tue wed thu fri sat sun)
 
-    ADDRESSES = %w(ave, bld, blvd, cl, ct, cres, dr, rd, st)
+    ADDRESSES = %w(ave bld blvd cl ct cres dr rd st)
 
-    MISC = %w(vs, etc, no, esp, cf)
+    MISC = %w(vs etc no esp cf)
 
     def self.list
-      TITLES + ENTITIES + MONTHS + DAYS + ADDRESSES + MISC
+      list = TITLES + ENTITIES + MONTHS + DAYS + ADDRESSES + MISC
+      list = list + list.map { |a| a.capitalize} 
     end
 
   end
